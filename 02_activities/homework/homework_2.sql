584d6f98330e5746f16e0467b68fdccedf431323
Tazeen Q <tazeenqureshi@gmail.com>
2024-09-12 18:18:58 -0400

    Update homework_2.sql

--- 02_activities/homework/homework_2.sql
+++ 02_activities/homework/homework_2.sql
@@ -5,7 +5,7 @@ SELECT *
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-SELECT customer_id, customer_first_name, customer_last_name, customer_zip
+SELECT *
   FROM customer
 ORDER BY customer_last_name, customer_first_name
  LIMIT 10;
